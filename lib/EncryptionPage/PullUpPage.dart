31701b77709a71872f16bbefacea1d9f4a19908d
khvitri <khuu.tr@northeastern.edu>
2022-11-06 13:59:21 -0500

    Fixed firebase configuration

--- lib/EncryptionPage/PullUpPage.dart
+++ lib/EncryptionPage/PullUpPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webpage/Functions/Encrypt.dart';
 import 'package:flutter_webpage/Functions/Upload.dart';
 
 class PullUpPage extends StatefulWidget {
@@ -7,9 +8,10 @@ class PullUpPage extends StatefulWidget {
 }
 
 class _PullUpPageState extends State<PullUpPage> {
-  dynamic file;
-
   List<Widget> pageChildren(double width, BuildContext context) {
+    dynamic file;
+    String? txt_to_str;
+    late var encrypted_str;
     return <Widget>[
       Container(
           width: width,
@@ -20,10 +22,15 @@ class _PullUpPageState extends State<PullUpPage> {
                   color: Color.fromARGB(255, 19, 126, 38),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                  onPressed: () {
-                    setState(() async {
-                      file = await Upload().selectFile();
-                    });
+                  onPressed: () async {
+                    file = await Upload().selectFile();
+                    if (file != null) {
+                      txt_to_str = String.fromCharCodes(file.file);
+                      encrypted_str = Encryption.encryptAES(txt_to_str);
+                      //TODO: Upload encrypted_str to firebase
+                      //TODO: Save the key as a text file to their computer
+                    }
+                    setState(() async {});
                   },
                   child: Text(
                     "Pull Up",
